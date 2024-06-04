6f384e762597137a8936293ca6afb786115e9aa1
devsamuelsilva <devsamuelsilva@gmail.com>
2024-06-04 20:44:36 -0300

    Criando as paginas de login e cadastrar junto com as rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import Login from './pages/login/Login.jsx';
+import NewUser from './pages/newUser/NewUser.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+  {
+    path: "/singup",
+    element: <NewUser/>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
